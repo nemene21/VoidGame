d4d6d8830a2f0b62fb9b8a03d23fe01374c72680
nemene21 <borna.ozanic@gmail.com>
2024-08-10 11:50:53 +0200

    Gun Z layering and flipping

--- src/game/entities/weapons/guns/gun.cpp
+++ src/game/entities/weapons/guns/gun.cpp
@@ -2,9 +2,9 @@
 
 Gun::Gun(int player_id, float firerate, float burst_delay, bool automatic, std::string texture, ShotPattern pattern):
     ProjectileWeapon(player_id, firerate, burst_delay, automatic, texture, pattern),
-    player_dist {8},
+    player_dist {10},
     height {8},
-    recoil_anim_strength {5}
+    recoil_anim_strength {6}
     {
     anim_comp = new AnimationComponent(this);
     anim_comp->make_animation("shoot", 0.2f, false);
@@ -34,8 +34,11 @@ void Gun::private_process(float delta) {
     // Interpolates position towards player
     trans_comp->position = Lerpi(
         trans_comp->position,
-        player_pos - Vector2{0.f, height}, 15);
+        player_pos - Vector2{0.f, height}, 30);
     
     // Z layering
     sprite.z_coord = (int)(player_pos.y < mouse_pos().y);
+
+    // Flipping gun when on left
+    trans_comp->scale.y = mouse_pos().x > player_pos.x ? 1 : -1;
 }
