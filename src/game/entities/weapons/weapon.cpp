3713fcfc96891458d8e2308eae00d3f7fee6acca
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-09-27 16:43:26 +0200

    Can't reload weapon being swapped anymore

--- src/game/entities/weapons/weapon.cpp
+++ src/game/entities/weapons/weapon.cpp
@@ -54,6 +54,8 @@ void Weapon::private_process(float delta) {
     // Scale spawn animation
     float blend = Easing::back_in(intro_anim);
     trans_comp->scale = {blend, blend};
+
+    timer_comp->get_timer("reload")->paused = active;
 }
 
 // Takes care of automatic/non automatic cases
