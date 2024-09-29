9afce4a4c645be0be9b2fe83dc905db322cf729b
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2024-09-29 13:32:15 +0200

    Made BG shader uniforms set up once

--- src/game/entities/background.cpp
+++ src/game/entities/background.cpp
@@ -12,17 +12,7 @@ Background::Background():
 
     add_component(new TransformComponent(this));
     set_name("Background");
-}
 
-void Background::process(float delta) {
-    auto cam = CameraManager::get_camera();
-    hole_sprite.update_transform((TransformComponent*)get_component(CompType::TRANSFORM));
-    hole_sprite.position += cam->target - cam->offset + half_res;
-
-    star_sprite.update_transform((TransformComponent*)get_component(CompType::TRANSFORM));
-    star_sprite.position += cam->target - cam->offset + half_res;
-
-    auto& bond = hole_sprite.shader_bond;
     Vector2 center = {(320.f/180.f) * .5f, .5};
     bond.send_uniform("center", &center, sizeof(center), SHADER_UNIFORM_VEC2);
     float radius = 0.3;
@@ -40,4 +30,14 @@ void Background::process(float delta) {
 
     bond.bind_texture("noise", TextureManager::get("black_hole/noise.png"));
     bond.bind_texture("hole_colors", TextureManager::get("black_hole/hole_gradient.png"));
+
+}
+
+void Background::process(float delta) {
+    auto cam = CameraManager::get_camera();
+    hole_sprite.update_transform((TransformComponent*)get_component(CompType::TRANSFORM));
+    hole_sprite.position += cam->target - cam->offset + half_res;
+
+    star_sprite.update_transform((TransformComponent*)get_component(CompType::TRANSFORM));
+    star_sprite.position += cam->target - cam->offset + half_res;
 }
