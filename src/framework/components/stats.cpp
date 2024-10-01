ac4c6e0b5a04a6c6bfc15a79100eba71f8660502
Brreks <brekslovro01@gmail.com>
2024-10-01 18:44:53 +0200

    Stats refactored ig

--- src/framework/components/stats.cpp
+++ src/framework/components/stats.cpp
@@ -1,35 +1,26 @@
 #include "stats.hpp"
 #include <iostream>
 
-void Stats::new_stat(const std::string& name, float value, float multiplier) {
-    stat[name] = value;
-    this->multiplier[name] = multiplier;
+void Stats::newstat(const std::string& name, Stat stat) {
+    stats[name] = stat;
 }
 
-void Stats::fetch_stat(const std::string& name) const {
-    if (stat.find(name) != stat.end()) {
-        float value = stat.at(name);
-        value *= multiplier.count(name) ? multiplier.at(name) : 1.0f;
-        std::cout << "Stat: " << name << "Value: " << value << std::endl;
+void Stats::fetchstat(const std::string& name) const {
+    if (stats.find(name) != stats.end()) {
+        std::cout << "Name: " <<
+        << "\n" << "Value: " <<
+        << "\n" << "Modifier: " << << std::endl;
     }
 }
 
-void Stats::new_value(const std::string& name, float value) {
-    if (stat.find(name) != stat.end()) {
-        stat[name] = value;
+void Stats::newvalue(const std::string& name, float value) {
+    if (stats.find(name) != stats.end()) {
+        stats[name].value = value;
     }
 }
 
-void Stats::new_mult(const std::string& name, float multiplierValue) {
-    if (multiplier.find(name) != multiplier.end()) {
-        multiplier[name] = multiplierValue;
+void Stats::newmult(const std::string& name, float multiplier) {
+    if (stats.find(name) != stats.end()) {
+        stats[name].multiplier = multiplier;
     }
-}
-
-void Stats::add_value(const std::string& name, float incrvalue) {
-    stat[name] += incrvalue;
-}
-
-void Stats::add_mult(const std::string& name, float incrmult) {
-    multiplier[name] += incrmult;
 }
